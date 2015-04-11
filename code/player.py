ae647eece43f6f08fb6c52b4f46981de15ce6e46
EmilyOwens <emily.f.owens@gmail.com>
2015-04-11 11:38:02 -0600

    Fixed platform deletion upon collision

--- code/player.py
+++ code/player.py
@@ -19,15 +19,19 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
          
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, True)
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         for block in block_hit_list:
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            elif self.change_x < 0:
-                self.rect.left = block.rect.right
+            if self.change_x > 0 and self.rect.bottom == block.rect.top:
+                self.change_x = 0
+                # 
+            elif self.change_x < 0 and self.rect.bottom == block.rect.top:
+                self.change_x = 0
+                # self.rect.left = block.rect.right
             if self.change_y > 0:
+                self.change_y = 0
                 self.rect.bottom = block.rect.top
             elif self.change_y < 0:
+                self.change_y = 0
                 self.rect.top = block.rect.bottom
                 
             self.change_y = 0
@@ -51,7 +55,7 @@ class Player(pygame.sprite.Sprite):
         self.change_x = 0
     def jump(self):
         self.rect.y += 2
-        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, True)
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         self.rect.y -= 2
         
         if len(platform_hit_list) > 0 or self.rect.bottom >= main.SCREEN_HEIGHT:
