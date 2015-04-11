96a739eeb43bb2bf8006a1960d31f1892cf2ecf1
Aeowynn <earthsea14@gmail.com>
2015-04-11 00:59:59 -0600

    Work in progress!

---
+++ code/titleScreen.py
@@ -0,0 +1,16 @@
+import pygame
+
+#Globals
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+pygame.draw.rect(screen, BLACK, [175, 125, 450, 400], 2)
+font = pygame.font.SysFont('Calibri', 25, True, False)
+self.text = font.render("HACK_WOLVES", True, BLACK)
+screen.blit(self.text, [(350),(250)])
