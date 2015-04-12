8f8fd09367a8a14db2a7866524423d19ea10d8bc
Aeowynn <earthsea14@gmail.com>
2015-04-11 19:30:28 -0600

    Finished unimplemented; renamed a button

--- code/unimplementedScreen.py
+++ code/unimplementedScreen.py
@@ -2,33 +2,17 @@ import pygame
 from helpers import *
 
 def unimplemented(done, screen, clock):
-    click = False
     transitionScreen = "titleScreen"
+    clicked = False
     while not done:
-        (mouseX, mouseY) = (0, 0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             if event.type == pygame.MOUSEBUTTONDOWN:
-                (mouseX, mouseY) = pygame.mouse.get_pos()
-                click = True
-                print "Mousex:", mouseX, "MouseY:", mouseY
-                if mouseX > 100 and mouseX < 350:
-                    if mouseY > 400 and mouseY < 475:
-                        transitionScreen = "blobScreen"
-                    elif mouseY > 500 and mouseY < 625:
-                        print "Load Save Button"
-                elif mouseX > 425 and mouseX < 675:
-                    if mouseY > 400 and mouseY < 475:
-                        print "Options Button"
-                        transitionScreen = "optionsScreen"
-                    elif mouseY > 500 and mouseY < 625:
-                        transitionScreen = "done"
+                clicked = True
+        
         screen.fill(BACKGROUND)
-        StartButton = TransitionButton(100, 400, "Start Game", screen)
-        LoadButton = TransitionButton(100, 500, "Load Save", screen)
-        OptionsButton = TransitionButton(425, 400, "Options", screen)
-        ExitButton = TransitionButton(425, 500, "Exit", screen)
+        ExitButton = TransitionButton(425, 500, "Main Menu", screen)
         
         TitleFont = pygame.font.SysFont('Calibri', 60, True, False)
         TitleText = TitleFont.render("This function isn't implemented yet;", True, BLACK)
@@ -36,7 +20,7 @@ def unimplemented(done, screen, clock):
         screen.blit(TitleText, [(50),(100)])
         screen.blit(SubtitleText, [(50),(200)])
         
-        if transitionScreen != None and click == True:
+        if transitionScreen != None and clicked == True:
             return transitionScreen
         
         clock.tick(60)
