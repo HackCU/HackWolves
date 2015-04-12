4b464ff56a822f03621c5196c4040412ee146294
CzarcasticJack <joshua.weaver@colorado.edu>
2015-04-11 19:53:57 -0600

    Escape exits on title

--- code/titleScreen.py
+++ code/titleScreen.py
@@ -9,6 +9,9 @@ def titleScreen(done, screen, clock):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 (mouseX, mouseY) = pygame.mouse.get_pos()
                 #print "Mousex:", mouseX, "MouseY:", mouseY
